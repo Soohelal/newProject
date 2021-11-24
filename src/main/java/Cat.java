3fde69b08938c87077edc8c8a239e72149aed6c7
olsmi <etolcons@mail.ru>
2021-11-24 19:59:07 +0300

    добавлен класс Cat

---
+++ src/main/java/Cat.java
@@ -0,0 +1,26 @@
+public class Cat extends Animal {
+
+    Cat name;
+    Cat age;
+
+    public Cat getName() {
+        return name;
+    }
+
+    public void setName(Cat name) {
+        this.name = name;
+    }
+
+    public Cat getAge() {
+        return age;
+    }
+
+    public void setAge(Cat age) {
+        this.age = age;
+    }
+
+    public Cat(Cat name, Cat age) {
+        this.name = name;
+        this.age = age;
+    }
+}
